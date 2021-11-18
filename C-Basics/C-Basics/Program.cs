0eee8c2565adab8c4218ec821c6b8b28804c8c84
Evan Frabell <epfrabell@gmail.com>
2021-11-18 13:35:15 -0500

    Initial Setup

--- C-Basics/C-Basics/Program.cs
+++ C-Basics/C-Basics/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// XML template
 namespace C_Basics
 {
     class Program
@@ -11,7 +12,11 @@ namespace C_Basics
         // Main entry point necessary to run application
         static void Main(string[] args)
         {
+            // Write to console
             Console.WriteLine("Goodbye, Pluto!");
+            // Keep console open to await user entry or I/O stream
+            Console.Read();
         }
     }
 }
+
