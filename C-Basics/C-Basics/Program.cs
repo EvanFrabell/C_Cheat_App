bec53fe79ac9b5770e950666705bf988f1ca2e9f
Evan Frabell <epfrabell@gmail.com>
2021-11-18 13:19:00 -0500

    Initial commit

---
+++ C-Basics/C-Basics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Basics
+{
+    class Program
+    {
+        // Main entry point necessary to run application
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Goodbye, Pluto!");
+        }
+    }
+}
